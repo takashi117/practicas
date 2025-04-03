67057481bf9f645eb1304e4ffbe460775e803d1d
takashi117 <119278093+takashi117@users.noreply.github.com>
2025-04-03 15:51:45 -0600

    Update funcionBurbujaRec.py

--- funcionBurbujaRec/funcionBurbujaRec/funcionBurbujaRec.py
+++ funcionBurbujaRec/funcionBurbujaRec/funcionBurbujaRec.py
@@ -3,9 +3,10 @@ import random
 import json
 import matplotlib.pyplot as plt
 sys.setrecursionlimit(2000)
-
+#Datos globales
 contador = 0
 datos = []
+
 def burbuja_rec(arreglo, n):
     global contador
     if n > 1:
@@ -36,29 +37,23 @@ def cargar_datos():
         with open("datos.json", "r") as f:
             datos = json.load(f)
         ltuplas = [tuple(par) for par in datos]
-        print("Datos almacenados:", ltuplas)
+        print("\nDatos almacenados (tamanno del problema , operaciones realizadas):\n", ltuplas)
     except FileNotFoundError:
         print("No hay datos almacenados.")
 
-
-listaTam=[10,50,100,500,1000]
+#funcion principal
+listaTam=[10,50,100,200,500,1000]
+condImprimir=False
+if listaTam[len(listaTam)-1]<=1000:
+    condImprimir=True
+    
 for n in listaTam:
-	tamanno_problema = n
-	lista_random = [random.randint(0, 1000) for _ in range(tamanno_problema)]
-	operaciones = burbuja(lista_random)
-	guardar_datos(tamanno_problema, operaciones)
-
-contador = 0
-print(f" el contador es: {operaciones}")
+    tamanno_problema = n
+    lista_random=[random.randint(0, 1000) for _ in range(tamanno_problema)]
+    if condImprimir == True:
+        print(f"\nlista en desorden:\n{lista_random}")
+    operaciones = burbuja(lista_random)
+    if condImprimir == True:
+        print(f"\nlista ordenada:\n{lista_random} con un contador: {operaciones}")
+    guardar_datos(tamanno_problema, operaciones)
 cargar_datos()
-
-#lgrafica=[(random.randint(0,10),random.randint(0,10)) for _ in range(10)]
-#lgrafica=[(operaciones, tamanno_problema) for _ in range(10)]
-#vx=[x for x,y in ltuplas]
-#vy=[y for x,y in ltuplas]
-#plt.plot(vx, vy, marka='o')
-#plt.xlabel("tam. problema")
-#plt.ylabel("operaciones")
-#plt.title("algoritmo")
-#plt.grid(True)
-#plt.show()
