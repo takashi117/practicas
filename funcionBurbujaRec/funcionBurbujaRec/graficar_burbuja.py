672dd06fbcc402ba3b875916f6f7e8e9acfa1a08
takashi117 <119278093+takashi117@users.noreply.github.com>
2025-04-07 16:36:36 -0600

    Update graficar_burbuja.py

--- funcionBurbujaRec/funcionBurbujaRec/graficar_burbuja.py
+++ funcionBurbujaRec/funcionBurbujaRec/graficar_burbuja.py
@@ -4,7 +4,7 @@ import matplotlib.pyplot as plt
 import json
 import os  # Para verificar si el archivo existe
 
-# Pedir al usuario qué archivo quiere graficar
+# Pedir al usuario que archivo quiere graficar
 archivo = input("Ingrese el nombre del archivo a graficar (datos.json o datos_b.json): ")
 
 # Verificar si el archivo existe antes de abrirlo
@@ -24,7 +24,7 @@ else:
     else:
         print(f"Datos cargados desde {archivo}: {ltuplas}")
 
-        # Separar los datos en listas para la gráfica
+        # Separar los datos en listas para la grafica
         vx = [x for x, y in ltuplas]
         vy = [y for x, y in ltuplas]
 
@@ -32,7 +32,7 @@ else:
         plt.plot(vx, vy, marker='o', label=archivo)
         plt.xlabel("Tam. problema")
         plt.ylabel("Operaciones")
-        plt.title(f"Comparación de {archivo}")
+        plt.title(f"Comparacion de {archivo}")
         plt.legend()
         plt.grid(True)
         plt.show()
